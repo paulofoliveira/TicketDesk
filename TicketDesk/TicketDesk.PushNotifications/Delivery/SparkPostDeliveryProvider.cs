a85cb5628c0c21c8267125fbfccb5c3ceadcdd6f
paulofoliveira <paulo.fdo@hotmail.com>
2017-08-01 00:01:55 -0300

    SparkPostDeliveryProvider added.

---
+++ TicketDesk/TicketDesk.PushNotifications/Delivery/SparkPostDeliveryProvider.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json.Linq;
+using S22.Mail;
+using SparkPost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TicketDesk.Localization;
+using TicketDesk.Localization.PushNotifications;
+using TicketDesk.PushNotifications.Model;
+
+// FONTE: https://github.com/darrencauthon/csharp-sparkpost
+
+namespace TicketDesk.PushNotifications.Delivery
+{
+    [LocalizedDescription("SparkPostProvider", NameResourceType = typeof(Strings))]
+    public class SparkPostDeliveryProvider : EmailDeliveryProviderBase
+    {
+        public SparkPostDeliveryProvider(JToken configuration)
+        {
+            Configuration = configuration == null ?
+                new SparkPostDeliveryProviderConfiguration() :
+                configuration.ToObject<SparkPostDeliveryProviderConfiguration>();
+        }
+
+        public override async Task<bool> SendNotificationAsync(PushNotificationItem notificationItem, object message, CancellationToken ct)
+        {
+            var cfg = (SendGridDeliveryProviderConfiguration)Configuration;
+            var sent = false;
+
+            MailMessage smsg = message as SerializableMailMessage;
+
+            if (smsg != null)
+            {
+                try
+                {
+                    var hView = smsg.AlternateViews.First(v => v.ContentType.MediaType == "text/html");
+                    var tView = smsg.AlternateViews.First(v => v.ContentType.MediaType == "text/plain");
+
+                    Transmission transmission = new Transmission();
+                    transmission.Content.From.Email = cfg.FromAddress;
+                    transmission.Content.From.Name = cfg.FromDisplayName;
+
+                    transmission.Content.Subject = smsg.Subject;
+                    transmission.Content.Text = tView.ContentStream.ReadToString();
+                    transmission.Content.Html = hView.ContentStream.ReadToString();
+
+                    transmission.Recipients.Add(new Recipient
+                    {
+                        Address = new Address { Email = notificationItem.Destination.DestinationAddress, Name = notificationItem.Destination.SubscriberName }
+                    });
+
+                    var client = new Client(cfg.ApiKey);
+                    await client.Transmissions.Send(transmission);
+                    sent = true;
+                }
+                catch (Exception)
+                {
+                    sent = false;
+                    //TODO: log this somewhere
+                }
+            }
+
+
+            return sent;
+        }
+    }
+}
---
+++ TicketDesk/TicketDesk.PushNotifications/Delivery/SparkPostDeliveryProviderConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TicketDesk.Localization;
+using TicketDesk.Localization.PushNotifications;
+
+namespace TicketDesk.PushNotifications.Delivery
+{
+    public class SparkPostDeliveryProviderConfiguration : IDeliveryProviderConfiguration
+    {
+        [Display(Name = "ApiKey", ResourceType = typeof(Strings))]
+        [LocalizedDescription("ApiKey_Description", NameResourceType = typeof(Strings))]
+        [Required(ErrorMessageResourceName = "FieldRequired", ErrorMessageResourceType = typeof(Validation))]
+        [DataType(DataType.Password)]
+        public string ApiKey { get; set; }
+
+        [Display(Name = "FromAddress", ResourceType = typeof(Strings))]
+        [LocalizedDescription("FromAddress_Description", NameResourceType = typeof(Strings))]
+        [Required(ErrorMessageResourceName = "FieldRequired", ErrorMessageResourceType = typeof(Validation))]
+        [DataType(DataType.EmailAddress, ErrorMessageResourceName = "InvalidEmail", ErrorMessageResourceType = typeof(Validation))]
+        public string FromAddress { get; set; }
+
+        [Display(Name = "FromDisplayName", ResourceType = typeof(Strings))]
+        [LocalizedDescription("FromDisplayName_Description", NameResourceType = typeof(Strings))]
+        [Required(ErrorMessageResourceName = "FieldRequired", ErrorMessageResourceType = typeof(Validation))]
+        public string FromDisplayName { get; set; }
+    }
+}
